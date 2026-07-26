beebdac9639b96ac30052ecef11ebd3b6976e5ef
msrasouli <msrasouli@yahoo.com>
2026-07-26 21:35:23 +0330

    Update Add contact.py

--- Add contact.py
+++ Add contact.py
@@ -1,10 +1,15 @@
 name = input("Enter name :")
 lastname = input("Enter Lastname :")
 phonenumber = input("Enter phone number :")
-note = input("Do you want write any notes ?")
+note = input("Do you want add any notes ?")
 
 if note == "yes" :
     note1=input("Give me note :")
-    print(name , lastname , phonenumber , note1 )
+    print(f"Name: {name}")
+    print(f"lastname: {lastname}")
+    print(f"phone number: {phonenumber}")
+    print(f"note: {note1}")
 else : 
-    print(name , lastname , phonenumber )
+    print(f"Name :{name}")
+    print(f"lastname: {lastname}")
+    print(f"phone number: {phonenumber}" )
